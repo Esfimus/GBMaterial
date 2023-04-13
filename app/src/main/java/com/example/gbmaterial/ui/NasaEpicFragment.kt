e824bf3c25cee820119a4b53728f03188bebf2a1
Nikolay N. Nikolaev <esfimus@gmail.com>
2023-04-13 20:30:00 +0300

    EPIC API is fully functional

--- app/src/main/java/com/example/gbmaterial/ui/NasaEpicFragment.kt
+++ app/src/main/java/com/example/gbmaterial/ui/NasaEpicFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import coil.ImageLoader
+import coil.request.ImageRequest
 import com.example.gbmaterial.databinding.FragmentNasaEpicBinding
 
 class NasaEpicFragment : Fragment() {
@@ -31,8 +34,35 @@ class NasaEpicFragment : Fragment() {
     private fun initView() {
         model.loadNasaEpic()
         model.epicLive.observe(viewLifecycleOwner) {
-            ui.epicText.text = it.toString()
+            val date = it[0].date
+            val imageId = it[0].image
+            val coordinates = """lat: ${it[0].centroidCoordinates.lat}
+                |lon: ${it[0].centroidCoordinates.lon}""".trimMargin()
+            imageBuilder(date, imageId)
+            with (ui) {
+                dateView.text = it[0].date
+                coordinatesView.text = coordinates
+            }
         }
+    }
+
+    private fun imageBuilder(date: String, imageId: String) {
+        try {
+            val dateList = date.split("""[-\s]""".toRegex())
+            val url = "https://epic.gsfc.nasa.gov/archive/natural/${dateList[0]}/${dateList[1]}/${dateList[2]}/jpg/$imageId.jpg"
+            ui.imageView.loadPicture(url)
+        } catch (_: java.lang.Exception) { }
+    }
+
+    private fun ImageView.loadPicture(url: String) {
+        val imageLoader = ImageLoader.Builder(this.context).build()
+        val request = ImageRequest.Builder(this.context)
+            .crossfade(true)
+            .crossfade(500)
+            .data(url)
+            .target(this)
+            .build()
+        imageLoader.enqueue(request)
     }
 
     override fun onDestroyView() {
